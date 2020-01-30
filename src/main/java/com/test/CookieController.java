913ea2daa7081816e1405ae25ca6680b4343c02c
gkgaurav31 <gkgaurav31@gmail.com>
2020-01-30 12:12:54 +0530

    tst

---
+++ src/main/java/com/test/CookieController.java
@@ -0,0 +1,29 @@
+package com.test;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CookieController {
+
+	@GetMapping("/")
+	public String readCookie(@CookieValue(value = "username", defaultValue = "DefaultName") String username) {
+	    return "Hey! My username is " + username;
+	}
+	
+	@GetMapping("/change")
+	public String setCookie(HttpServletResponse response, @RequestParam String username) {
+	    // create a cookie
+	    Cookie cookie = new Cookie("username", username);
+	    //add cookie to response
+	    response.addCookie(cookie);
+	    cookie.setSecure(true);
+	    return "Username is changed to " + username + "!";
+	}
+	
+}
